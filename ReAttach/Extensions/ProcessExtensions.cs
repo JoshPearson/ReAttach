70fe5d7736d83d81c945d8e7ae6f85dcd6c51ec2
Josh Pearson <josh.pearson@outlook.com>
2024-05-20 13:39:08 -0600

    Adds support for saving & filtering by username on remote targets

--- ReAttach/Extensions/ProcessExtensions.cs
+++ ReAttach/Extensions/ProcessExtensions.cs
@@ -4,36 +4,40 @@ using System;
 
 namespace ReAttach.Extensions
 {
-	public static class ProcessExtensions
-	{
-		public static string GetUsername(this Process3 process)
-		{
-			var name = process.UserName;
-			if (string.IsNullOrEmpty(name))
-				return name;
-			var start = name.LastIndexOf('[');
-			return start != -1 ? name.Substring(0, start).TrimEnd() : name;
-		}
+   public static class ProcessExtensions
+   {
+      public static string GetUsername(this Process3 process)
+      {
+         var name = process.UserName;
+         if (string.IsNullOrEmpty(name))
+            return name;
+         var start = name.LastIndexOf('[');
+         return start != -1 ? name.Substring(0, start).TrimEnd() : name;
+      }
 
-		public static bool IsMatchingLocalProcess(this Process3 process, ReAttachTarget target)
-        {
-			return
-				string.Compare(process.Name, target.ProcessPath, StringComparison.OrdinalIgnoreCase) == 0 &&
-				string.Compare(process.UserName, target.ProcessUser) == 0;
-		}
+      public static bool IsMatchingLocalProcess(this Process3 process, ReAttachTarget target)
+      {
+         return
+            string.Compare(process.Name, target.ProcessPath, StringComparison.OrdinalIgnoreCase) == 0 &&
+            string.Compare(process.UserName, target.ProcessUser) == 0;
+      }
 
-		public static bool IsMatchingExclusively(this Process3 process, ReAttachTarget target)
-        {
-			if (!string.IsNullOrEmpty(process.UserName))
-				return false;
+      public static bool IsMatchingExclusively(this Process3 process, ReAttachTarget target)
+      {
+         if (!string.IsNullOrEmpty(process.UserName))
+            return false;
 
-			return string.Compare(process.Name, target.ProcessName, StringComparison.OrdinalIgnoreCase) == 0;
-		}
+         return string.Compare(process.Name, target.ProcessName, StringComparison.OrdinalIgnoreCase) == 0;
+      }
 
-		public static bool IsMatchingRemoteProcess(this Process3 process, ReAttachTarget target)
-        {
-			return string.Compare(process.Name, target.ProcessPath, StringComparison.OrdinalIgnoreCase) == 0;
-		}
+      public static bool IsMatchingRemoteProcess(this Process3 process, ReAttachTarget target)
+      {
+         //Does not take the process username into consideration :
+         //return string.Compare(process.Name, target.ProcessPath, StringComparison.OrdinalIgnoreCase) == 0;
+         return string.Equals(process.Name, target.ProcessPath, StringComparison.OrdinalIgnoreCase) 
+            && string.Equals(process.UserName, target.ProcessUser, StringComparison.OrdinalIgnoreCase);
 
-	}
+      }
+
+   }
 }
--- ReAttach/Services/ReAttachDebugger.cs
+++ ReAttach/Services/ReAttachDebugger.cs
@@ -16,261 +16,263 @@ using ReAttach.Stores;
 
 namespace ReAttach.Services
 {
-    public enum ReAttachResult
-    {
-        Success,
-        NotStarted,
-        ElevationRequired,
-        Cancelled,
-        Failed,
-    }
+   public enum ReAttachResult
+   {
+      Success,
+      NotStarted,
+      ElevationRequired,
+      Cancelled,
+      Failed,
+   }
 
-    public class ReAttachDebugger: IVsDebuggerEvents, IDebugEventCallback2
-    {
-        private ReAttachHistory _history;
-        private ReAttachUi _ui;
-        private Debugger2 _dteDebugger;
-        private uint _cookie;
-        private Dictionary<Guid, string> _engines;
-        private bool _recording;
+   public class ReAttachDebugger : IVsDebuggerEvents, IDebugEventCallback2
+   {
+      private ReAttachHistory _history;
+      private ReAttachUi _ui;
+      private Debugger2 _dteDebugger;
+      private uint _cookie;
+      private Dictionary<Guid, string> _engines;
+      private bool _recording;
 
-        public async Task InitializeAsync(ReAttachPackage package, ReAttachHistory history, CancellationToken cancellationToken)
-        {
-            _history = history;
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-
-            _ui = (await package.GetServiceAsync(typeof(ReAttachUi))) as ReAttachUi;
-            if (_ui == null)
-            {
-                ReAttachUtils.ShowStartupError("Unable to obtain reference to UI.");
-                return;
-            }
-
-            var debugger = (await package.GetServiceAsync(typeof(IVsDebugger))) as IVsDebugger;
-            if (debugger == null) 
-            {
-                ReAttachUtils.ShowStartupError("Unable to obtain reference to debugger.");
-                return;
-            }
-
-            if (debugger.AdviseDebugEventCallback(this) != VSConstants.S_OK)
-            {
-                ReAttachUtils.ShowStartupError("Unable to subscribe on debug events.");
-                return;
-            }
-
-            if (debugger.AdviseDebuggerEvents(this, out _cookie) != VSConstants.S_OK)
-            {
-                ReAttachUtils.ShowStartupError("Unable to subscribe on debugger mode changes.");
-                return;
-            }
+      public async Task InitializeAsync(ReAttachPackage package, ReAttachHistory history, CancellationToken cancellationToken)
+      {
+         _history = history;
+         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
 
-            var dte = await package.GetServiceAsync(typeof(EnvDTE.DTE)) as DTE2;
-            if (dte == null)
-            {
-                ReAttachUtils.ShowStartupError("Unable to get obtain reference to automation object model (DTE2).");
-                return;
-            }
+         _ui = (await package.GetServiceAsync(typeof(ReAttachUi))) as ReAttachUi;
+         if (_ui == null)
+         {
+            ReAttachUtils.ShowStartupError("Unable to obtain reference to UI.");
+            return;
+         }
 
-            _dteDebugger = dte.Debugger as Debugger2;
-            if (_dteDebugger == null)
-            {
-                ReAttachUtils.ShowStartupError("Unable to get reference to debugger from automation object.");
-                return;
-            }
+         var debugger = (await package.GetServiceAsync(typeof(IVsDebugger))) as IVsDebugger;
+         if (debugger == null)
+         {
+            ReAttachUtils.ShowStartupError("Unable to obtain reference to debugger.");
+            return;
+         }
 
-            _engines = GetTransportEngines();
-        }
+         if (debugger.AdviseDebugEventCallback(this) != VSConstants.S_OK)
+         {
+            ReAttachUtils.ShowStartupError("Unable to subscribe on debug events.");
+            return;
+         }
 
-        public int OnModeChange(DBGMODE mode)
-        {
-            if (_history == null)
-                return VSConstants.S_OK;
+         if (debugger.AdviseDebuggerEvents(this, out _cookie) != VSConstants.S_OK)
+         {
+            ReAttachUtils.ShowStartupError("Unable to subscribe on debugger mode changes.");
+            return;
+         }
 
-            switch (mode)
-            {
-                case DBGMODE.DBGMODE_Run:
-                    _recording = true;
-                    break;
-                case DBGMODE.DBGMODE_Design:
-                    _recording = false;
-                    _history.Save();
-                    break;
-            }
+
+         var dte = await package.GetServiceAsync(typeof(EnvDTE.DTE)) as DTE2;
+         if (dte == null)
+         {
+            ReAttachUtils.ShowStartupError("Unable to get obtain reference to automation object model (DTE2).");
+            return;
+         }
+
+         _dteDebugger = dte.Debugger as Debugger2;
+         if (_dteDebugger == null)
+         {
+            ReAttachUtils.ShowStartupError("Unable to get reference to debugger from automation object.");
+            return;
+         }
+
+         _engines = GetTransportEngines();
+      }
+
+      public int OnModeChange(DBGMODE mode)
+      {
+         if (_history == null)
             return VSConstants.S_OK;
-        }
 
-        public int Event(IDebugEngine2 engine, IDebugProcess2 process, IDebugProgram2 program, IDebugThread2 thread, IDebugEvent2 evt, ref Guid riidEvent, uint dwAttrib)
-        {
-            if (!_recording) return VSConstants.S_OK;
+         switch (mode)
+         {
+            case DBGMODE.DBGMODE_Run:
+               _recording = true;
+               break;
+            case DBGMODE.DBGMODE_Design:
+               _recording = false;
+               _history.Save();
+               break;
+         }
+         return VSConstants.S_OK;
+      }
 
-            if (!(evt is IDebugProcessCreateEvent2) &&
-                !(evt is IDebugProcessDestroyEvent2) &&
-                !(evt is IDebugEntryPointEvent2))
-                return VSConstants.S_OK;
+      public int Event(IDebugEngine2 engine, IDebugProcess2 process, IDebugProgram2 program, IDebugThread2 thread, IDebugEvent2 evt, ref Guid riidEvent, uint dwAttrib)
+      {
+         if (!_recording) return VSConstants.S_OK;
 
-            var target = GetTargetFromProcess(process);
-            if (target == null)
-            {
-
-                //_package.Reporter.ReportWarning("Can't find target from process {0} ({1}). Event: {2}.",
-                //  process.GetName(), process.GetProcessId(), TypeHelper.GetDebugEventTypeName(debugEvent));
-                return VSConstants.S_OK;
-            }
-
-            if (evt is IDebugProcessCreateEvent2 || evt is IDebugEntryPointEvent2)
-            {
-                target.IsAttached = true;
-                _history.AddFirst(target);
-                _ui.Update();
-            }
-            else
-            {
-                target.IsAttached = false;
-                _ui.Update();
-            }
-
+         if (!(evt is IDebugProcessCreateEvent2) &&
+             !(evt is IDebugProcessDestroyEvent2) &&
+             !(evt is IDebugEntryPointEvent2))
             return VSConstants.S_OK;
-        }
 
-        public ReAttachResult ReAttach(ReAttachTarget target)
-        {
-            if (target == null)
-                return ReAttachResult.Failed;
+         var target = GetTargetFromProcess(process);
+         if (target == null)
+         {
 
-            List<Process3> candidates;
-            if (!target.IsLocal)
-            {
-                var transport = _dteDebugger.Transports.Item("Default");
-                var processes = _dteDebugger.GetProcesses(transport, target.ServerName).OfType<Process3>();
-                candidates = processes.Where(p => p.IsMatchingRemoteProcess(target)).ToList();
-            }
-            else
-            {
-                var processes = _dteDebugger.LocalProcesses.OfType<Process3>();
-                candidates = processes.Where(p => p.IsMatchingLocalProcess(target)).ToList();
-                if (!candidates.Any()) // Do matching on processes running in exclusive mode.
-                {
-                    candidates = processes.Where(p => p.IsMatchingExclusively(target)).ToList();
-                }
-            }
+            //_package.Reporter.ReportWarning("Can't find target from process {0} ({1}). Event: {2}.",
+            //  process.GetName(), process.GetProcessId(), TypeHelper.GetDebugEventTypeName(debugEvent));
+            return VSConstants.S_OK;
+         }
 
-            if (!candidates.Any())
-                return ReAttachResult.NotStarted;
+         if (evt is IDebugProcessCreateEvent2 || evt is IDebugEntryPointEvent2)
+         {
+            target.IsAttached = true;
+            _history.AddFirst(target);
+            _ui.Update();
+         }
+         else
+         {
+            target.IsAttached = false;
+            _ui.Update();
+         }
 
-            Process3 process = null; // First try to use the pid.
-            if (target.ProcessId > 0)
-                process = candidates.FirstOrDefault(p => p.ProcessID == target.ProcessId);
+         return VSConstants.S_OK;
+      }
 
-            // If we don't have an exact match, just go for the highest PID matching.
-            if (process == null)
-            {
-                var maxPid = candidates.Max(p => p.ProcessID);
-                process = candidates.FirstOrDefault(p => p.ProcessID == maxPid);
-            }
-
-            if (process == null)
-                return ReAttachResult.NotStarted;
-
-            try
-            {
-                if (target.Engines != null && target.Engines.Any())
-                {
-                    var engines = target.Engines.Where(e => _engines.ContainsKey(e)).Select(e => _engines[e]).ToArray();
-                    process.Attach2(engines);
-                }
-                else
-                {
-                    process.Attach();
-                }
-                return ReAttachResult.Success;
-            }
-            catch (COMException)
-            {
-                return ReAttachResult.ElevationRequired;
-            }
-            catch (Exception e)
-            {
-                ReAttachUtils.ShowError($"Unable to ReAttach to process {process.Name} ({process.ProcessID}) based on target {target}", e.Message);
-            }
+      public ReAttachResult ReAttach(ReAttachTarget target)
+      {
+         if (target == null)
             return ReAttachResult.Failed;
-        }
 
-
-        private Dictionary<Guid, string> GetTransportEngines()
-        {
-            var engines = new Dictionary<Guid, string>();
-            try
+         List<Process3> candidates;
+         if (!target.IsLocal)
+         {
+            var transport = _dteDebugger.Transports.Item("Default");
+            var processes = _dteDebugger.GetProcesses(transport, target.ServerName).OfType<Process3>();
+            candidates = processes.Where(p => p.IsMatchingRemoteProcess(target)).ToList();
+         }
+         else
+         {
+            var processes = _dteDebugger.LocalProcesses.OfType<Process3>();
+            candidates = processes.Where(p => p.IsMatchingLocalProcess(target)).ToList();
+            if (!candidates.Any()) // Do matching on processes running in exclusive mode.
             {
-                var ignoredEngines = new HashSet<Guid>(new[] { new Guid("2c18241e-069a-43b2-bd81-89c186af994b") });
-
-                foreach (Transport transport in _dteDebugger.Transports)
-                {
-                    foreach (Engine engine in transport.Engines)
-                    {
-                        var engineId = Guid.Parse(engine.ID);
-                        if (ignoredEngines.Contains(engineId) || engines.ContainsKey(engineId))
-                            continue;
-
-                        engines.Add(engineId, engine.Name);
-                    }
-                }
+               candidates = processes.Where(p => p.IsMatchingExclusively(target)).ToList();
             }
-            catch (Exception e)
+         }
+
+         if (!candidates.Any())
+            return ReAttachResult.NotStarted;
+
+         Process3 process = null; // First try to use the pid.
+         if (target.ProcessId > 0)
+            process = candidates.FirstOrDefault(p => p.ProcessID == target.ProcessId);
+
+         // If we don't have an exact match, just go for the highest PID matching.
+         if (process == null)
+         {
+            var maxPid = candidates.Max(p => p.ProcessID);
+            process = candidates.FirstOrDefault(p => p.ProcessID == maxPid);
+         }
+
+         if (process == null)
+            return ReAttachResult.NotStarted;
+
+         try
+         {
+            if (target.Engines != null && target.Engines.Any())
             {
-                ReAttachUtils.ShowStartupError("Unable to get debugging engines for transports: " + e.Message);
+               var engines = target.Engines.Where(e => _engines.ContainsKey(e)).Select(e => _engines[e]).ToArray();
+               process.Attach2(engines);
             }
-            return engines;
-        }
-
-        private ReAttachTarget GetTargetFromProcess(IDebugProcess2 debugProcess)
-        {
-            var process = (IDebugProcess3)debugProcess;
-            var pid = process.GetProcessId();
-            var target = _history.FindByPid(pid);
-            if (target != null)
-                return target;
-
-
-            var serverName = "";
-            IDebugCoreServer2 server;
-            if (debugProcess.GetServer(out server) == VSConstants.S_OK)
+            else
             {
-                var server3 = server as IDebugCoreServer3;
-                var tmp = "";
-                if (server3 != null && server3.QueryIsLocal() == VSConstants.S_FALSE &&
-                    server3.GetServerFriendlyName(out tmp) == VSConstants.S_OK)
-                {
-                    serverName = tmp;
-                }
+               process.Attach();
             }
+            return ReAttachResult.Success;
+         }
+         catch (COMException)
+         {
+            return ReAttachResult.ElevationRequired;
+         }
+         catch (Exception e)
+         {
+            ReAttachUtils.ShowError($"Unable to ReAttach to process {process.Name} ({process.ProcessID}) based on target {target}", e.Message);
+         }
+         return ReAttachResult.Failed;
+      }
 
-            var user = GetProcessUsername(pid);
-            var path = process.GetFilename();
-            target = new ReAttachTarget(pid, path, user, serverName);
 
-            var rawEngines = new GUID_ARRAY[1];
+      private Dictionary<Guid, string> GetTransportEngines()
+      {
+         var engines = new Dictionary<Guid, string>();
+         try
+         {
+            var ignoredEngines = new HashSet<Guid>(new[] { new Guid("2c18241e-069a-43b2-bd81-89c186af994b") });
 
-            if (process.GetEngineFilter(rawEngines) == VSConstants.S_OK && rawEngines[0].dwCount > 0)
+            foreach (Transport transport in _dteDebugger.Transports)
             {
-                var pointer = rawEngines[0].Members;
-                var engineCount = rawEngines[0].dwCount;
-                for (var i = 0; i < engineCount; i++)
-                {
-                    var engineId = (Guid)Marshal.PtrToStructure(pointer + (i * 16), typeof(Guid));
-                    target.Engines.Add(engineId);
-                }
+               foreach (Engine engine in transport.Engines)
+               {
+                  var engineId = Guid.Parse(engine.ID);
+                  if (ignoredEngines.Contains(engineId) || engines.ContainsKey(engineId))
+                     continue;
+
+                  engines.Add(engineId, engine.Name);
+               }
             }
+         }
+         catch (Exception e)
+         {
+            ReAttachUtils.ShowStartupError("Unable to get debugging engines for transports: " + e.Message);
+         }
+         return engines;
+      }
+
+      private ReAttachTarget GetTargetFromProcess(IDebugProcess2 debugProcess)
+      {
+         var process = (IDebugProcess3)debugProcess;
+         var pid = process.GetProcessId();
+         var target = _history.FindByPid(pid);
+         if (target != null)
             return target;
-        }
 
-        private string GetProcessUsername(int pid)
-        {
-            var process = _dteDebugger.LocalProcesses.OfType<Process3>().FirstOrDefault(p => p.ProcessID == pid);
-            var result = process != null ? process.UserName : string.Empty;
-            return result;
-        }
-    }
+
+         var serverName = "";
+         IDebugCoreServer2 server;
+         if (debugProcess.GetServer(out server) == VSConstants.S_OK)
+         {
+            var server3 = server as IDebugCoreServer3;
+            var tmp = "";
+            if (server3 != null && server3.QueryIsLocal() == VSConstants.S_FALSE &&
+                server3.GetServerFriendlyName(out tmp) == VSConstants.S_OK)
+            {
+               serverName = tmp;
+            }
+         }
+
+         var user = GetProcessUsername(pid);
+         var path = process.GetFilename();
+         target = new ReAttachTarget(pid, path, user, serverName);
+
+         var rawEngines = new GUID_ARRAY[1];
+
+         if (process.GetEngineFilter(rawEngines) == VSConstants.S_OK && rawEngines[0].dwCount > 0)
+         {
+            var pointer = rawEngines[0].Members;
+            var engineCount = rawEngines[0].dwCount;
+            for (var i = 0; i < engineCount; i++)
+            {
+               var engineId = (Guid)Marshal.PtrToStructure(pointer + (i * 16), typeof(Guid));
+               target.Engines.Add(engineId);
+            }
+         }
+         return target;
+      }
+
+      private string GetProcessUsername(int pid)
+      {
+         //Fails to get the username of a remote process:
+         //var process = _dteDebugger.LocalProcesses.OfType<Process3>().FirstOrDefault(p => p.ProcessID == pid);
+         var process = _dteDebugger.DebuggedProcesses.OfType<Process3>().FirstOrDefault(p => p.ProcessID == pid);
+         var result = process != null ? process.UserName : string.Empty;
+         return result;
+      }
+   }
 }
